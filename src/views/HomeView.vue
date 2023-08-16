4f020781a0504848af81ba0e9df60bd9e8e3acea
temurbek-kdv <temur.kdv@gmail.com>
2023-08-16 20:13:54 +0500

    project created icons set

---
+++ src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import Header from "../components/common/Header.vue";
+import Sidebar from "../components/common/Sidebar.vue";
+import Footer from "../components/common/Footer.vue";
+</script>
+
+<template>
+  <Header></Header>
+  <Sidebar></Sidebar>
+  <div class="p-4 sm:ml-64">
+   <div class="p-4 mt-14">
+     <main>
+     </main>
+     <Footer></Footer>
+   </div>
+  </div> 
+</template>
